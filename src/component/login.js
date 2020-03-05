f0b3a7f43dd49c85fcdc26f59d49d3580108d4d2
nabaraj <nabaraj2k@gmail.com>
2020-03-05 23:55:00 +0530

    login modification

--- src/component/login.js
+++ src/component/login.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 
-function LoginUser() {
+function LoginUser(props) {
   return (
     <div className="login-page">
       <div className="form">
