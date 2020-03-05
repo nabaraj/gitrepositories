ef2a889c689a5f5ee14b36a68793e5b72228d9e1
nabaraj <nabaraj2k@gmail.com>
2020-03-05 23:54:25 +0530

    login modification

--- src/component/login.js
+++ src/component/login.js
@@ -5,22 +5,11 @@ function LoginUser() {
   return (
     <div className="login-page">
       <div className="form">
-        <form className="register-form">
-          <input type="text" placeholder="name" />
-          <input type="password" placeholder="password" />
-          <input type="text" placeholder="email address" />
-          <button>create</button>
-          <p className="message">
-            Already registered? <a href="#">Sign In</a>
-          </p>
-        </form>
         <form className="login-form">
+          <h3>Provide username and email to view repositories</h3>
           <input type="text" placeholder="username" />
-          <input type="password" placeholder="password" />
-          <button>login</button>
-          <p className="message">
-            Not registered? <a href="#">Create an account</a>
-          </p>
+          <input type="text" placeholder="email" />
+          <button>Submit</button>
         </form>
       </div>
     </div>
